f902d13bde89b00e46b9f6bf65993b05c74a7084
Khen Tungnung <49173538+CSProjects@users.noreply.github.com>
2020-10-12 22:43:51 -0400

    1st commit

---
+++ home.js
@@ -0,0 +1,6 @@
+function submitUsernameAndPassword(){
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+    //document.getElementById('displayunameandpword').innerHTML=username.value+password.value;
+}
+document.getElementById('submitbutton').onclick = submitUsernameAndPassword;
